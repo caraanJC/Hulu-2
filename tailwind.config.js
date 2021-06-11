fa962ece7fed9c3f938b1b2a4286e82c61e32cec
caraanJC <caraanjohncarlo@gmail.com>
2021-06-11 21:21:19 +0800

    feat: finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '2000px',
+      },
+    },
   },
   variants: {
     extend: {
       scale: ['hover'],
       textColor: ['active'],
+      textOverflow: ['hover', 'focus'],
     },
   },
   plugins: [require('tailwind-scrollbar-hide')],
